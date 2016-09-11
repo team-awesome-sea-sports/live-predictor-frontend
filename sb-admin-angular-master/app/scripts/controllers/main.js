1510263057de1fce5d5eca148671e0ac5b59099f
lwenke01 <lwenke@gmail.com>
2016-09-11 13:43:42 -0700

    got data to pull through angular

--- sb-admin-angular-master/app/scripts/controllers/main.js
+++ sb-admin-angular-master/app/scripts/controllers/main.js
@@ -1,28 +1,65 @@
 'use strict';
-/**
- * @ngdoc function
- * @name sbAdminApp.controller:MainCtrl
- * @description
- * # MainCtrl
- * Controller of the sbAdminApp
- */
+
 angular.module('sbAdminApp')
   // .controller('MainCtrl', function($scope,$position) {
   // });
+  // .config(['$httpProvider', function($httpProvider) {
+  //         $httpProvider.defaults.useXDomain = true;
+  //         $httpProvider.defaults.headers.common = 'Content-Type: application/json';
+  //         delete $httpProvider.defaults.headers.common['X-Requested-With'];
+  //     }
+  // ])
   .controller('MainCtrl', ['$http', '$scope', '$position', SchedController]);
   function SchedController($http, $scope, $position) {
+    var vm = this;
     const schedRoute = 'https://api.sportradar.us/nfl-t1/2016/reg/1/schedule.json?api_key=f88crygtpcxcnc5gu93sqw3m';
-    this.schedule = ['bleh'];
-    this.getSchedule = function() {
-      console.log('Anyone home?');
-      $http.get(schedRoute)
+    vm.schedules = ['bleh'];
+
+      $http({
+        method: 'GET',
+        url: 'https://api.sportradar.us/nfl-t1/2016/reg/1/schedule.json?api_key=f88crygtpcxcnc5gu93sqw3m'
+
+
+      })
+
+      // {
+      //
+      //
+      //   url: schedRoute,
+      //   dataType: 'jsonp'
+      // })
       .then((res) => {
-        this.schedule = res.data.data;
-        console.log(this.schedule);
+        console.log('res',res);
+        vm.schedules = res.data;
+        console.log('data is ',vm.schedules);
       },
       function(error) {
         console.error(error);
       }
     );
-  }
-}
+
+};
+
+// function LiveController($http, $scope, $position) {
+//   var vm = this;
+//   const schedRoute = 'https://api.sportradar.us/nfl-t1/2016/reg/1/schedule.json?api_key=f88crygtpcxcnc5gu93sqw3m';
+//   vm.liveScore = ['bleh'];
+//
+//     $http({
+//       method: 'GET',
+//       url: 'https://api.sportradar.us/nfl-t1/2016/reg/1/schedule.json?api_key=f88crygtpcxcnc5gu93sqw3m'
+//
+//
+//     })
+//
+//     .then((res) => {
+//       console.log('res',res);
+//       vm.schedules = res.data;
+//       console.log('data is ',vm.schedules);
+//     },
+//     function(error) {
+//       console.error(error);
+//     }
+//   );
+//
+// }
