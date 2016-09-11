755877bb3a7e59c36ad03fd076bb4419adf5aada
adam <adam.coon@gmail.com>
2016-09-11 15:30:14 -0400

    import

---
+++ ui-aws/scripts/read_queue_forever.js
@@ -0,0 +1,32 @@
+function read_one_message_from_queue(sqs_conn, sqs_arn, ui_callback_function){
+    sqs_conn.receiveMessage({
+        "QueueArn": sqs_arn,
+        "MaxNumberOfMessages": 1,
+        "VisibilityTimeout": 30,
+        "WaitTimeSeconds": 20
+    }, function (err, data) {
+        var sqs_message_body;
+        if (data.Messages) {
+            if (typeof data.Messages[0] !== 'undefined' && typeof data.Messages[0].Body !== 'undefined') {
+                //sqs msg body
+                sqs_message_body = JSON.parse(data.Messages[0].Body);
+        
+                //do something...
+                ui_callback_function(sqs_message_body);
+
+                //if it works, delete message from queue
+                sqs_conn.deleteMessage({
+                    "QueueArn" : sqs_arn,
+                    "ReceiptHandle" :data.Messages[0].ReceiptHandle
+                });
+            }
+            else { window.alert(err); console.log(err); }
+        }
+        else { window.alert(err); console.log(err); }
+    });
+}
+
+function read_messages_from_queue(sqs_conn, sqs_arn, ui_callback_function){
+    // repeat forever, polling every 10 seconds
+    window.setInterval(read_one_message_from_queue(sqs_conn, sqs_arn, ui_callback_function), 10000); 
+}
---
+++ ui-aws/scripts/ui-callbacks.js
@@ -0,0 +1,11 @@
+function new_situation(situation_data){
+    //TODO
+    alert("New Situation Data Received! " + situation_data);
+    $("#situation_msg").innerHTML = situation_data;
+}
+
+function new_scores(score_data){
+    //TODO
+    alert("New Scores Data Received! " + score_data);
+    $("#scores_msg").innerHTML = score_data;
+}
