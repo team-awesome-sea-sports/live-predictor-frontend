f2a84997557321ecdfc52e51c62257289a112b11
AJ Coon <ajcoon@gmail.com>
2016-09-11 17:34:32 -0400

    Debugging queue listener issues

--- ui-aws/scripts/read_queue_forever.js
+++ ui-aws/scripts/read_queue_forever.js
@@ -1,12 +1,15 @@
-function read_one_message_from_queue(sqs_conn, sqs_arn, ui_callback_function){
-    sqs_conn.receiveMessage({
-        "QueueArn": sqs_arn,
+function read_one_message_from_queue(sqs_conn, queue_url, ui_callback_function){
+    var receive_message_params = {
+        "QueueUrl": queue_url,
         "MaxNumberOfMessages": 1,
         "VisibilityTimeout": 30,
         "WaitTimeSeconds": 20
-    }, function (err, data) {
+    }
+    sqs_conn.receiveMessage(
+        receive_message_params,
+        function (err, data) {
         var sqs_message_body;
-        if (data.Messages) {
+        if (data && data.Messages) {
             if (typeof data.Messages[0] !== 'undefined' && typeof data.Messages[0].Body !== 'undefined') {
                 //sqs msg body
                 sqs_message_body = JSON.parse(data.Messages[0].Body);
@@ -16,18 +19,23 @@ function read_one_message_from_queue(sqs_conn, sqs_arn, ui_callback_function){
 
                 //if it works, delete message from queue
                 sqs_conn.deleteMessage({
-                    "QueueArn" : sqs_arn,
+                    "QueueUrl" : queue_url,
                     "ReceiptHandle" :data.Messages[0].ReceiptHandle
                 });
+            } else { 
+                alert("data: " + data + "data.Messages " + data.Messages);
             }
-            else { window.alert(err); console.log(err); }
+        } else {
+            setTimeout(function(){
+                read_one_message_from_queue(sqs_conn, queue_url, ui_callback_function)
+            }, 5000);
         }
-        else { window.alert(err); console.log(err); }
     });
 }
 
-function read_messages_from_queue(sqs_conn, sqs_arn, ui_callback_function){
-    // repeat forever, polling every 2 seconds
-    //alert("Checking for new messages on SQS arn: "+ sqs_arn);
-    window.setInterval(read_one_message_from_queue(sqs_conn, sqs_arn, ui_callback_function), 2000); 
+
+
+function read_messages_from_queue(sqs_conn, queue_url, ui_callback_function){
+    //alert("Checking for new messages on SQS URL: "+ queue_url);
+    read_one_message_from_queue(sqs_conn, queue_url, ui_callback_function);
 }
